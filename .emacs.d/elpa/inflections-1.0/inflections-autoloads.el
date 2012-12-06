1377d6afe206b0e618f603a1a5968fceef340592
Panagiotis Xinos <pxinos@skroutz.gr>
2012-12-06 15:22:31 +0200

    my emacs conf

---
+++ .emacs.d/elpa/inflections-1.0/inflections-autoloads.el
@@ -0,0 +1,34 @@
+;;; inflections-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (pluralize-string singularize-string) "inflections"
+;;;;;;  "inflections.el" (20662 14823 283150 440000))
+;;; Generated autoloads from inflections.el
+
+(autoload 'singularize-string "inflections" "\
+
+
+\(fn STR)" nil nil)
+
+(autoload 'pluralize-string "inflections" "\
+
+
+\(fn STR)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("inflections-pkg.el") (20662 14823 291491
+;;;;;;  326000))
+
+;;;***
+
+(provide 'inflections-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; inflections-autoloads.el ends here
