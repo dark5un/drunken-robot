b425e1b1cbf5f58995b4700494e55fa7576e105a
Panagiotis Xinos <panagiotis.xinos@gmail.com>
2013-03-07 11:08:37 +0200

    removing old emacs config

--- .emacs.d/elpa/idle-highlight-1.0/idle-highlight-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; idle-highlight-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (idle-highlight) "idle-highlight" "idle-highlight.el"
-;;;;;;  (20671 24777 511209 294000))
-;;; Generated autoloads from idle-highlight.el
-
-(autoload 'idle-highlight "idle-highlight" "\
-Toggle idle-highlighting.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("idle-highlight-pkg.el") (20671 24777
-;;;;;;  517734 332000))
-
-;;;***
-
-(provide 'idle-highlight-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; idle-highlight-autoloads.el ends here
